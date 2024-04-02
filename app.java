751df1704b9e7ed3bfcb9944656abe5bf2a25ff2
mariabdj <mariaboudjelal17@gmail.com>
2024-04-02 22:19:43 +0100

    finished all classes

--- app.java
+++ app.java
@@ -1,3 +1,11 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class app {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bonjour, \n si vous êtes un patient entrez P \n si vous êtes un médecin entrez M \n Si vous êtes le secrétaire entrez S ");
+        String start = scanner.next();
+    }
 }
